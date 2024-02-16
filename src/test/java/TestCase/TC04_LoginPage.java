9b6bcb764bc331274b0bd56ef8a6eabe2dbe2c25
Subhashb-1302 <158320171+Subhashb-1302@users.noreply.github.com>
2024-02-16 17:26:44 +0530

    Update TC04_LoginPage.java

--- src/test/java/TestCase/TC04_LoginPage.java
+++ src/test/java/TestCase/TC04_LoginPage.java
@@ -37,7 +37,7 @@ public class TC04_LoginPage extends BaseClass {
 		System.out.println("Error Message: " + errorMsg);
 		logger.info("Provide the invalid email inorder to get Error message");
 		
-		sa.assertEquals(errorMsg, "Couldn�t find your Google Account");
+		sa.assertEquals(errorMsg, "Couldn't find your Google Account");
 		sa.assertAll();
 		logger.info("Validating the error message displayed");
 
